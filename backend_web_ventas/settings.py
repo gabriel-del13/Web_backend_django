46781f5e009710d8423b570c2d53342d772be793
Gabriel <gaby13453@gmail.com>
2024-09-28 18:55:50 -0500

    test6

--- backend_web_ventas/settings.py
+++ backend_web_ventas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # BASE_DIR es la ruta base del proyecto (la carpeta principal que contiene todos los archivos del proyecto).
